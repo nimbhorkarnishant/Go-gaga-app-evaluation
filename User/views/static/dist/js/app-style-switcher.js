629ee7557e3409707f8c8e8d491492d65b71b9e0
Nishant Nimbhorkar <36923081+nimbhorkarnishant@users.noreply.github.com>
2020-05-23 20:00:11 +0530

    commit

--- User/views/static/dist/js/app-style-switcher.js
+++ User/views/static/dist/js/app-style-switcher.js
@@ -1,17 +1,13 @@
 $(function() {
     "use strict";
 
-    //****************************
-    /* Top navbar Theme Change function Start */
-    //****************************
+
     function handlenavbarbg() {
         if ( $('#main-wrapper').attr('data-navbarbg') == 'skin6' ) {
-            // do this
             $(".topbar .navbar").addClass('navbar-light');
             $(".topbar .navbar").removeClass('navbar-dark');
         } else {
-            // do that    
-        }    
+        }
     };
 
     handlenavbarbg();
--- User/views/static/dist/js/sidebarmenu.js
+++ User/views/static/dist/js/sidebarmenu.js
@@ -1,12 +1,3 @@
-/*
-Template Name: Admin Template
-Author: Wrappixel
-
-File: js
-*/
-// ============================================================== 
-// Auto select left navbar
-// ============================================================== 
 $(function() {
     "use strict";
      var url = window.location + "";
@@ -26,26 +17,26 @@ $(function() {
             else if(!$(this).is("ul") && $(this).children("a").length === 0)
             {
                 $(this).addClass("selected");
-                
+
             }
             else if($(this).is("ul")){
                 $(this).addClass('in');
             }
-            
+
         });
 
-    element.addClass("active"); 
+    element.addClass("active");
     $('#sidebarnav a').on('click', function (e) {
-        
+
             if (!$(this).hasClass("active")) {
                 // hide any open menus and remove all other classes
                 $("ul", $(this).parents("ul:first")).removeClass("in");
                 $("a", $(this).parents("ul:first")).removeClass("active");
-                
+
                 // open our new menu and add the open class
                 $(this).next("ul").addClass("in");
                 $(this).addClass("active");
-                
+
             }
             else if ($(this).hasClass("active")) {
                 $(this).removeClass("active");
@@ -56,5 +47,5 @@ $(function() {
     $('#sidebarnav >li >a.has-arrow').on('click', function (e) {
         e.preventDefault();
     });
-    
+
 });
